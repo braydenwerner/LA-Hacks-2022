61a6901ff467842624ac499779469e2f5d967b9a
aidenszeto <aszeto35@gmail.com>
2022-04-16 19:36:54 -0700

    barebones splash page

---
+++ client/src/components/modules/SplashPage/SplashPage.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const SplashContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  height: 100%;
+  width: 100%;`
